24a23f2adc40c0a4ea04fe4428188d7fe1bd01cb
Rohit Ingole <ingobingo3491@gmail.com>
2022-01-17 23:53:03 +0530

    streamlit app ready

--- streamlit-app/helpers/predictor.py
+++ streamlit-app/helpers/predictor.py
@@ -9,7 +9,7 @@ from tensorflow.keras.preprocessing import image
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 def predict(img):
-    model = keras.models.load_model("./model_31-0.96.h5")
+    model = keras.models.load_model("model_31-0.96.h5")
 
     # Load the Image
     img = Image.open(img)
