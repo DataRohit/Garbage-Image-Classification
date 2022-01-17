c872e094126b87a1d7e34e73581c7319d82ef3e4
Rohit Ingole <ingobingo3491@gmail.com>
2022-01-18 00:16:20 +0530

    streamlit app ready

--- streamlit-app/pages/app2.py
+++ streamlit-app/pages/app2.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from helpers import predictor
 
@@ -7,14 +8,14 @@ def app():
     st.write("## Sample Images")
 
     col1, col2, col3 = st.columns(3)
-    col1.image("https://i.ibb.co/XWq96B1/cardboard.jpg", caption='0 : Cardboard')
-    col2.image("https://i.ibb.co/9Y3hscx/glass.jpg", caption='1 : Glass')
-    col3.image("https://i.ibb.co/2ZHxnc7/metal.jpg", caption='2 : Metal')
+    col1.image(os.path.join(os.getcwd(), "streamlit-app", "samples", "cardboard.jpg"), caption='0 : Cardboard')
+    col2.image(os.path.join(os.getcwd(), "streamlit-app", "samples", "glass.jpg"), caption='1 : Glass')
+    col3.image(os.path.join(os.getcwd(), "streamlit-app", "samples", "metal.jpg"), caption='2 : Metal')
 
     col4, col5, col6 = st.columns(3)
-    col4.image("https://i.ibb.co/pbvjX4Y/paper.jpg", caption='3 : Paper')
-    col5.image("https://i.ibb.co/Wczj3VH/plastic.jpg", caption='4 : Plastic')
-    col6.image("https://i.ibb.co/GdxYMFp/trash.jpg", caption='5 : Trash')
+    col4.image(os.path.join(os.getcwd(), "streamlit-app", "samples", "paper.jpg"), caption='3 : Paper')
+    col5.image(os.path.join(os.getcwd(), "streamlit-app", "samples", "plastic.jpg"), caption='4 : Plastic')
+    col6.image(os.path.join(os.getcwd(), "streamlit-app", "samples", "trash.jpg"), caption='5 : Trash')
 
     st.write("## Upload Image in .jpg format")
     uploaded_image = st.file_uploader("", type=["jpg"])
