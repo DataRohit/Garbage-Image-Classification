e56863a256271cbd9c19597d43696ef0e7382756
Rohit Ingole <ingobingo3491@gmail.com>
2022-01-17 23:30:34 +0530

    streamlit app ready

--- streamlit-app/pages/app2.py
+++ streamlit-app/pages/app2.py
@@ -7,14 +7,14 @@ def app():
     st.write("## Sample Images")
 
     col1, col2, col3 = st.columns(3)
-    col1.image("./samples/cardboard.jpg", caption='0 : Cardboard')
-    col2.image("./samples/glass.jpg", caption='1 : Glass')
-    col3.image("./samples/metal.jpg", caption='2 : Metal')
+    col1.image("https://i.ibb.co/XWq96B1/cardboard.jpg", caption='0 : Cardboard')
+    col2.image("https://i.ibb.co/9Y3hscx/glass.jpg", caption='1 : Glass')
+    col3.image("https://i.ibb.co/2ZHxnc7/metal.jpg", caption='2 : Metal')
 
     col4, col5, col6 = st.columns(3)
-    col4.image("./samples/paper.jpg", caption='3 : Paper')
-    col5.image("./samples/plastic.jpg", caption='4 : Plastic')
-    col6.image("./samples/trash.jpg", caption='5 : Trash')
+    col4.image("https://i.ibb.co/pbvjX4Y/paper.jpg", caption='3 : Paper')
+    col5.image("https://i.ibb.co/Wczj3VH/plastic.jpg", caption='4 : Plastic')
+    col6.image("https://i.ibb.co/GdxYMFp/trash.jpg", caption='5 : Trash')
 
     st.write("## Upload Image in .jpg format")
     uploaded_image = st.file_uploader("", type=["jpg"])
@@ -28,6 +28,6 @@ def app():
 
         if button:
             predicted_class = predictor.predict(uploaded_image)
-            st.write(f"#### The Predicted Class for the Uploaded Image is : {predicted_class.capitalize()}")
+            st.markdown(f"<h4>{predicted_class.capitalize()} Detected!!</h4>", True)
     else:
         st.write("#### No Image Found. Pls Upload the Image.")
