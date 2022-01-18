8b6724650038744cfb7d69bb5dad9edf34870dd7
Rohit Ingole <ingobingo3491@gmail.com>
2022-01-18 12:06:45 +0530

    streamlit app ready

--- streamlit-app/pages/app1.py
+++ streamlit-app/pages/app1.py
@@ -21,7 +21,7 @@ def app():
 
     st.subheader("Motivation :")
     st.markdown("This Project is created for the <b>Hack4Good Competition</b> presented by <b>IEEE CIS SBC - GHRCE.</b>", True)
-    st.markdown("Visit the Hack4Good Home page <a href='https://hack4good.ieee-cis-sbc.org/' target='_blank' rel='noopener noreferrer'>`here`</a></b>", True)
+    st.markdown("Visit the Hack4Good Home page [Hack4Good](https://hack4good.ieee-cis-sbc.org/)", True)
 
     st.markdown("<hr/>", True)
 
@@ -33,7 +33,7 @@ def app():
 
     st.subheader("Data :")
     st.markdown("The Data used in this project is available on Kaggle.")
-    st.markdown("Visit Kaggle for Data <a href='https://www.kaggle.com/asdasdasasdas/garbage-classification' target='_blank' rel='noopener noreferrer'>`here`</a>", True)
+    st.markdown("Visit Kaggle for Data [Kaggle](https://www.kaggle.com/asdasdasasdas/garbage-classification)", True)
     st.markdown("The approximate size of the Data is 43MB. It contains `2,527 Images categorized in 6 classes.`", True)
 
     st.markdown("<hr/>", True)
@@ -62,5 +62,5 @@ def app():
     st.markdown("<hr/>", True)
 
     st.subheader("Code :")
-    st.markdown("Full code and files for this project are availabe on my github repository <a href='https://github.com/DataRohit/Garbage-Image-Classification' target='_blank' rel='noopener noreferrer'>`here`</a>.", True)
+    st.markdown("Full code and files for this project are availabe on my github repository [GitHub](https://github.com/DataRohit/Garbage-Image-Classification)", True)
     
